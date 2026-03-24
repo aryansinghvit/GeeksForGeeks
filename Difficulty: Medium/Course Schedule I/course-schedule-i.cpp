9c57ff731e58d45f48d4f78a73e749ae1b97fa57
Aryan Singh <130549374+aryansinghvit@users.noreply.github.com>
2026-03-24 21:09:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule I/course-schedule-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+        
+        // Build graph
+        for(auto &p : prerequisites) {
+            int x = p[0];
+            int y = p[1];
+            
+            adj[y].push_back(x); // y -> x
+            indegree[x]++;
+        }
+        
+        queue<int> q;
+        
+        // Push nodes with indegree 0
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        int count = 0;
+        
+        // BFS (Kahn's Algorithm)
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            
+            for(auto neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        
+        return count == n;
+    }
+};
