2d8e2186e7d06c367fc85ebea7a461a99d9d3764
Aryan Singh <130549374+aryansinghvit@users.noreply.github.com>
2026-03-29 22:45:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countPartitions(self, arr, diff):
+        total = sum(arr)
+        
+        if total < diff or (total + diff) % 2 != 0:
+            return 0
+            
+        target = (total + diff) // 2
+        
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for num in arr:
+            for j in range(target, num - 1, -1):
+                dp[j] += dp[j - num]
+                
+        return dp[target]
